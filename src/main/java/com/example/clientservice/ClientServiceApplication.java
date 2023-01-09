350a2b960651f2135b5631e9895d88015b69e79b
nizaralloul7 <nizaralloul7@outlook.fr>
2023-01-09 21:08:58 +0100

    heroku

--- src/main/java/com/example/clientservice/ClientServiceApplication.java
+++ src/main/java/com/example/clientservice/ClientServiceApplication.java
@@ -31,9 +31,10 @@ public class ClientServiceApplication {
 	@Bean
 	public CorsFilter corsFilter()
 	{
+		//transfere push
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
