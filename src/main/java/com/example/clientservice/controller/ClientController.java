c97cf674db0b610dad00fb9e6337be74287f0272
nizaralloul7 <nizaralloul7@outlook.fr>
2023-01-09 21:24:37 +0100

    heroku

--- src/main/java/com/example/clientservice/controller/ClientController.java
+++ src/main/java/com/example/clientservice/controller/ClientController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
+@CrossOrigin(origins = "*")
 public class ClientController
 {
     private final ClientRepository clientRepository;
