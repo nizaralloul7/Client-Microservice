3dc7643abcdfd30559fbf16d80962b4bbc3b5eab
Hamza <50715380+Hassenehamza@users.noreply.github.com>
2023-01-10 04:36:59 +0100

    .

--- src/main/java/com/example/clientservice/configuration/WebConfig.java
+++ src/main/java/com/example/clientservice/configuration/WebConfig.java
@@ -3,26 +3,17 @@ package com.example.clientservice.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-      }
-    };
+  @Override
+  public void addCorsMappings(CorsRegistry registry)
+  {
+    registry.addMapping("/**")
+            .allowedOrigins("**")
+            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
   }
 }
