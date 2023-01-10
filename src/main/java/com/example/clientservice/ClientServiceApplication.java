f2d136a88bf7b0a482eab02a99e2c737343d766a
Hamza <50715380+Hassenehamza@users.noreply.github.com>
2023-01-10 05:46:41 +0100

    .

--- src/main/java/com/example/clientservice/ClientServiceApplication.java
+++ src/main/java/com/example/clientservice/ClientServiceApplication.java
@@ -33,7 +33,7 @@ public class ClientServiceApplication {
 
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedOrigins(ImmutableList.of("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
